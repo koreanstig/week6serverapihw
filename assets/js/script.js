84d2631c0da3a8525e9f5c7a73e32b9978fb3b59
Jung Lee <koreanstig@gmail.com>
2021-02-02 15:28:56 -0800

    added to my function to show the data on current weather portion

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,10 @@ var currentCityUV = document.getElementById("currentCityUV");
 // variables for prev searches
 var prevSearches = document.getElementById("prevSearches");
 var prevCityArr = [];
-
+// this will fetch data from serverside api
 function currentCityApi() {
     var value = encodeURIComponent(localStorage.getItem("value"));
-    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + value + "&appid=" + apiKey;
+    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + value + "&appid=" + apiKey + "&units=imperial";
     console.log(requestUrl);
     fetch(requestUrl)
         .then(function(response){
@@ -21,18 +21,40 @@ function currentCityApi() {
                 console.log(response);
                 response.json().then(function(data){
                     console.log(data);
+                    var lat = data.coord.lat;
+                    var lon = data.coord.lon;
+                    var requestUVUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+                    fetch(requestUVUrl) 
+                        .then(function(response2){
+                            if (response2.ok) {
+                                response2.json().then(function(data){
+                                    console.log(data);
+                                    currentCityUV.textContent = "UV Index: " + data.value;
+                                })
+                            }
+                            else {
+                                return
+                            };
+                        });
+                    currentCity.textContent = data.name;
+                    currentCityTemp.textContent = "Temperature: " + data.main.temp + " *F";
+                    currentCityHumidity.textContent = "Humidity: " + data.main.humidity + "%";
+                    currentCityWind.textContent = "Wind Speed: " + data.wind.speed + " MPH";    
                 });
             }
             else {
                 alert('Please enter a valid city name')
             };
-        })
+        });
 }
-
+// this will activate the api function upon click and save input into local storage
 searchBtn.addEventListener('click', function(event){
     event.preventDefault();
     var value = cityInput.value;
     localStorage.setItem("value", value);
+    prevCityArr.push(value);
+    console.log(prevCityArr);
     currentCityApi();
 })
 
+// var displayPrevSearches = function 
